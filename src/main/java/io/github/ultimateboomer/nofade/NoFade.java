20b5b7e9f121499a5c6e1a48fe675a454eaf0502
Derpy <lukef72704@gmail.com>
2023-05-12 21:29:28 -0400

    new logo, change mod name to Fadeless

--- src/main/java/io/github/ultimateboomer/nofade/NoFade.java
+++ src/main/java/io/github/ultimateboomer/nofade/NoFade.java
@@ -5,8 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class NoFade implements ModInitializer {
-	public static final String MOD_ID = "nofadein";
-	public static final String MOD_NAME = "NoFadeIn";
+	public static final String MOD_NAME = "Fadeless";
 
 	public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 
