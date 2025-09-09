59d3bc8a373b4a9f6fb9be0c6b21a1d0f31e1737
Derpy <57187871+DerpDerpling@users.noreply.github.com>
2025-09-09 10:48:04 -0400

    remove fade in from loadingoverlay
    adds #10

--- src/main/java/derp/fadeless/mixin/LoadingOverlayMixin.java
+++ src/main/java/derp/fadeless/mixin/LoadingOverlayMixin.java
@@ -1,6 +1,8 @@
 package derp.fadeless.mixin;
 
 import derp.fadeless.config.FadelessConfig;
+import derp.fadeless.config.FadelessConfig.OverlayFade;
+import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.LoadingOverlay;
@@ -15,13 +17,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(LoadingOverlay.class)
 public abstract class LoadingOverlayMixin extends Overlay {
     @Shadow private long fadeOutStart;
-
-
     @Shadow @Final private Minecraft minecraft;
+    @Shadow private boolean fadeIn;
+    @Shadow private long fadeInStart;
 
     @Inject(method = "render", at = @At("HEAD"))
     private void onRender(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
-        if (this.fadeOutStart > 1 && FadelessConfig.overlayFade.equals(FadelessConfig.OverlayFade.DISABLED)) {
+        if (FadelessConfig.overlayFade == OverlayFade.DISABLED) {
+            if (this.fadeIn && this.fadeInStart == -1L) {
+                long now = Util.getMillis();
+                this.fadeInStart = now - 1000L;
+            }
+        }
+        if (this.fadeOutStart > 1L && FadelessConfig.overlayFade == OverlayFade.DISABLED) {
             this.minecraft.setOverlay(null);
         }
     }
